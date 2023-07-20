245a8b903709081e9b494eb04ac5c902d77d6af8
iammrverma <rajwebz2020@gmail.com>
2023-07-21 05:24:28 +0530

    header, footer and hero section of home tab completed

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from './Layout'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <footer className='w-full border-2 border-solid border-dark font-medium text-lg'>
+      <Layout className='py-8 flex items-center justify-between'>
+        <span>{new Date().getFullYear()} &copy; rights reserved</span>
+        <div className="">
+        <Link href='/'>iammrverma</Link>
+        </div>
+        <Link href='mailto:rajbez2020@gmail.com' className='underline'>Contact Now</Link>
+      </Layout>
+    </footer>
+  )
+}
+
+export default Footer
