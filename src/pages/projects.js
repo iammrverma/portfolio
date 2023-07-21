5d10be13eaca2378e280b1a5e9f6c1dab3fe89a1
iammrverma <rajwebz2020@gmail.com>
2023-07-21 18:05:20 +0530

    articles section completed

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,6 +7,10 @@ import React from "react";
 import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
 import Image from "next/image";
 
+import { motion } from "framer-motion";
+
+const FramerImage = motion(Image);
+
 const FeaturedProject = ({ type, title, summary, img, link, githubLink }) => {
   return (
     <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-dark p-12 relative rounded-br-2xl">
@@ -16,7 +20,10 @@ const FeaturedProject = ({ type, title, summary, img, link, githubLink }) => {
         target="_blank"
         className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
       >
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage src={img} alt={title} className="w-full h-auto" 
+                      whileHover={{scale:1.05}}
+            transition={{duration:0.2}}
+        />
       </Link>
 
       <div className="w-1/2 flex flex-col items-start justify-between pl-6 ">
@@ -55,7 +62,9 @@ const Project = ({title, type, img, link, githubLink}) =>{
         target="_blank"
         className="w-full cursor-pointer overflow-hidden rounded-lg"
       >
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage src={img} alt={title} className="w-full h-auto"
+                    whileHover={{scale:1.05}}
+            transition={{duration:0.2}} />
       </Link>
 
       <div className="w-full flex flex-col items-start justify-between mt-4">
