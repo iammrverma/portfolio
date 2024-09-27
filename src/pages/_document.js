f66ca2fa6fd41548dccd29d0fa84e47f6ff10657
Raj Verma <rajwebz2020@gmail.com>
2024-09-27 12:35:17 +0530

    added clarity code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,16 +4,29 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Microsoft Clarity Tracking Code */}
+        <Script
+          id="clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "o9mxcn3ix7");`,
+          }}
+        />
+      </Head>
       <body>
         <Script id="theme-switcher" strategy="beforeInteractive">
           {`
-        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark')
-} else {
-  document.documentElement.classList.remove('dark')
-}
-        `}
+            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+              document.documentElement.classList.add('dark')
+            } else {
+              document.documentElement.classList.remove('dark')
+            }
+          `}
         </Script>
         <Main />
         <NextScript />
