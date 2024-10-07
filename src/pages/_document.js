bbe260fe2fc59f310964c760e49f1e13800caba4
Raj Verma <rajwebz2020@gmail.com>
2024-10-07 12:54:28 +0530

    Update _document.js: removing clarity code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,20 +4,6 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Microsoft Clarity Tracking Code */}
-        <Script
-          id="clarity"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "o9mxcn3ix7");`,
-          }}
-        />
-      </Head>
       <body>
         <Script id="theme-switcher" strategy="beforeInteractive">
           {`
