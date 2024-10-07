8f5fb57c95c0856c60688e9878ac380d8adb1410
Raj Verma <rajwebz2020@gmail.com>
2024-10-07 12:25:23 +0530

    Update _app.js : adding GA code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,8 @@ import { AnimatePresence } from "framer-motion";
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -13,12 +15,44 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-BCSZBY3J2P', {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {/* Google Analytics Script */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=G-BCSZBY3J2P`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BCSZBY3J2P');
+          `,
+        }}
+      />
+
       <main
         className={`${montserrat.variable} dark:bg-dark font-mont bg-light w-full min-h-screen`}
       >
